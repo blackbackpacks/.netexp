77e62aa633e074ad061695156bcb84bb29dcbe39
blackbackpacks <1017926959@qq.com>
2019-01-23 15:54:37 +0800

    add "controller" and routemap

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "this is my <b>default</b> 动作";
+        }
+        //Get: HelloWorld/Welcome
+        public string Welcome(string name, int ID)
+        {
+            return HttpUtility.HtmlEncode(" 好吧，" + name + "你抓到我了" + ID);
+        }
+    }
+}
